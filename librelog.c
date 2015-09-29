416ef3240eeada5a5bd402d65e2e0acc7fc697d2
Nicolas Carrier <nicolas.carrier@parrot.com>
2015-09-29 18:19:14 +0200

    [DEV] code factoring and allow to redirect error to a process

--- librelog.c
+++ librelog.c
@@ -9,95 +9,141 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define LIBRELOG_OUTFILE_ENVIRONMENT "RELOG_OUTFILE"
 #define LIBRELOG_ERRFILE_ENVIRONMENT "RELOG_ERRFILE"
 
 #define LIBRELOG_OUTPROCESS_ENVIRONMENT "RELOG_OUTPROCESS"
+#define LIBRELOG_ERRPROCESS_ENVIRONMENT "RELOG_ERRPROCESS"
 
-static FILE *out_process_pipe = NULL;
-static int old_stdout_fileno = -1;
+struct relog_process {
+	const char *cmd;
+	FILE *pipe;
+	int old_fd_value;
+	int old_fd_backup;
+};
+
+static struct relog_process out_process = {
+		.old_fd_value = STDOUT_FILENO,
+		.old_fd_backup = -1,
+};
+static struct relog_process err_process = {
+		.old_fd_value = STDERR_FILENO,
+		.old_fd_backup = -1,
+};
+
+/* file: file to redirect to, fd: stream redirected */
+/* no _cleanup, the normal C standard streams cleanup will take place */
+static int relog_file_init(const char *file, int fd)
+{
+	int newfd;
+	int ret = -1;
+
+	newfd = open(file, O_WRONLY);
+	if (newfd == -1) {
+		fprintf(stderr, "%d open: %m\n", __LINE__);
+		goto err;
+	}
+
+	ret = dup2(newfd, fd);
+	if (ret == -1) {
+		fprintf(stderr, "%d dup2: %m\n", __LINE__);
+		goto err;
+	}
+
+	return 0;
+err:
+	if (newfd != -1)
+		close(newfd);
+
+	return ret;
+}
+
+static void relog_process_clean(struct relog_process *process)
+{
+	if (process->pipe == NULL)
+		return;
+
+	/* close the dup'ed fd of the process, for pclose will terminate it */
+	close(process->old_fd_value);
+	pclose(process->pipe);
+
+	if (process->old_fd_backup == -1)
+		return;
+
+	dup2(process->old_fd_backup, process->old_fd_value);
+	close(process->old_fd_backup);
+}
+
+static int relog_process_init(struct relog_process *process)
+{
+	int ret = -1;
+
+	if (process->cmd == NULL || *process->cmd == '\0') {
+		fprintf(stderr, "%d %s: %s\n", __LINE__, __func__,
+				strerror(EINVAL));
+		return -1;
+	}
+	process->pipe = popen(process->cmd, "w");
+	if (process->pipe == NULL) {
+		fprintf(stderr, "%d popen: %m\n", __LINE__);
+		return -1;
+	}
+	/* backup the standard file descriptor */
+	process->old_fd_backup = dup(process->old_fd_value);
+	if (process->old_fd_backup == -1) {
+		fprintf(stderr, "%d dup: %m\n", __LINE__);
+		goto err;
+	}
+	ret = dup2(fileno(process->pipe), process->old_fd_value);
+	if (ret == -1) {
+		fprintf(stderr, "%d dup2: %m\n", __LINE__);
+		goto err;
+	}
+
+	return 0;
+err:
+	relog_process_clean(process);
+
+	return ret;
+}
 
 __attribute__((constructor)) void init_relog(void)
 {
 	const char *old_ld_preload;
-	int ret;
-	int outfd = -1;
-	int errfd = -1;
 	const char *outfile = getenv(LIBRELOG_OUTFILE_ENVIRONMENT);
 	const char *errfile = getenv(LIBRELOG_ERRFILE_ENVIRONMENT);
-	const char *outprocess = getenv(LIBRELOG_OUTPROCESS_ENVIRONMENT);
+
+	if (outfile != NULL)
+		relog_file_init(outfile, STDOUT_FILENO);
+	if (errfile != NULL)
+		relog_file_init(errfile, STDERR_FILENO);
 
 	/* avoid creating a fork bomb, hahem... */
 	old_ld_preload = getenv("LD_PRELOAD");
 	if (old_ld_preload != NULL)
 		unsetenv("LD_PRELOAD");
 
-	if (outfile != NULL) {
-		outfd = open(outfile, O_WRONLY);
-		if (outfd == -1) {
-			fprintf(stderr, "%d open: %m\n", __LINE__);
-			goto err;
-		}
-
-		ret = dup2(outfd, STDOUT_FILENO);
-		if (ret == -1) {
-			fprintf(stderr, "%d dup2: %m\n", __LINE__);
-			goto err;
-		}
-	}
-	if (outprocess != NULL) {
-
-		out_process_pipe = popen(outprocess, "w");
-		if (outprocess == NULL) {
-			fprintf(stderr, "%d popen: %m\n", __LINE__);
-			goto err;
-		}
-		/* backup 1 */
-		old_stdout_fileno = dup(STDOUT_FILENO);
-		if (old_stdout_fileno == -1) {
-			fprintf(stderr, "%d dup: %m\n", __LINE__);
-			goto err;
-		}
-		ret = dup2(fileno(out_process_pipe), STDOUT_FILENO);
-		if (ret == -1) {
-			fprintf(stderr, "%d dup2: %m\n", __LINE__);
-			goto err;
-		}
-	}
-	if (errfile != NULL) {
-		errfd = open(errfile, O_WRONLY);
-		if (errfd == -1) {
-			fprintf(stderr, "%d open: %m\n", __LINE__);
-			goto err;
-		}
-
-		ret = dup2(errfd, STDERR_FILENO);
-		if (ret == -1) {
-			fprintf(stderr, "%d dup2: %m\n", __LINE__);
-			goto err;
-		}
-	}
+	out_process.cmd = getenv(LIBRELOG_OUTPROCESS_ENVIRONMENT);
+	if (out_process.cmd != NULL)
+		relog_process_init(&out_process);
+	err_process.cmd = getenv(LIBRELOG_ERRPROCESS_ENVIRONMENT);
+	if (err_process.cmd != NULL)
+		relog_process_init(&err_process);
 
 	setlinebuf(stderr);
 	setlinebuf(stdout);
 
 	if (old_ld_preload != NULL)
 		setenv("LD_PRELOAD", old_ld_preload, 1);
-
-	return;
-err:
-	if (outfd != -1)
-		close(outfd);
-	if (errfd != -1)
-		close(errfd);
 }
 
-__attribute__((destructor)) void clean_relog(void)
+static __attribute__((destructor)) void relog_clean(void)
 {
-	if (out_process_pipe != NULL)
-		pclose(out_process_pipe);
-	dup2(old_stdout_fileno, STDOUT_FILENO);
+	relog_process_clean(&out_process);
+	relog_process_clean(&err_process);
 }
